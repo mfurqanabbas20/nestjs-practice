6c627e62c324f6ad487df294125867479eb6a7c6
mfurqanabbas00 <furqan.abbas@teamstack360.co>
2025-11-18 15:26:53 +0500

    feat: add basic structure for items module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsController } from './items/items.controller';
 import { AuthController } from './auth/auth.controller';
+import { ItemsService } from './items/items.service';
 
 @Module({
   imports: [],
   controllers: [AppController, ItemsController, AuthController],
-  providers: [AppService],
+  providers: [AppService, ItemsService],
 })
 export class AppModule {}
